db9f046ffe2587144ea2598d8069aa90c19cd9ef
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-04-30 17:56:13 +0200

    Sentencia_insert_desde_Python_a_MySQL_terminada

--- insertar_personas.py
+++ insertar_personas.py
@@ -12,3 +12,11 @@ personas_db = mysql.connector.connect(
 # EJECUTAMOS LA SENTECNIA INSERT
 cursor = personas_db.cursor()
 sentencia_sql = 'INSERT INTO personas(nombre, apellido, edad) VALUES(%s, %s, %s)'
+valores = ('Andres', 'Ramos', 46)
+cursor.execute(sentencia_sql, valores)
+personas_db.commit()  # guardamos los cambios en la BD
+print(f'Se a agregado el nuevo registro en la bd: {valores}')
+
+# Cerramos los recursos
+cursor.close()
+personas_db.close()
