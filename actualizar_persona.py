8dd4460e3bb176593a276a05ccd80f644b0047e2
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-05-01 20:49:18 +0200

    Sentencia_update_ejecutada

--- actualizar_persona.py
+++ actualizar_persona.py
@@ -2,9 +2,15 @@
 
 import mysql.connector
 
+from insertar_personas import sentencia_sql
+
 personas_db = mysql.connector.connect(
     host='localhost',  # 127.0.0.1
     user='root',
     password='admin',
     database='personas_db'
 )
+# ejecutamos la sencia update
+cursor = personas_db.cursor()
+sentencia_sql = 'UPDATE personas SET nombre =%s, apellido=%s, edad =%s WhERE id=%s'
+valores = ('Victoria', 'Flores', 45, 6)
