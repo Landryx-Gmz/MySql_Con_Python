a97cfbbbf67a3169982dc3564902051bbf2d039c
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-03 11:43:23 +0200

    Comentarios_en_eliminar_persona.py

--- 4._eliminar_persona.py
+++ 4._eliminar_persona.py
@@ -8,12 +8,17 @@ personas_db = mysql.connector.connect(
     password='admin',
     database='personas_db'
 )
-# Ejecutamos la sencia delete
+# Ejecutamos la sencia DELETE
 cursor = personas_db.cursor()
+# Objeto de sentencia DELETE
 sentencia_sql = 'DELETE FROM personas WHERE id=%s'
+# Proporcionamos el valor en el mismo orden
 valores = (7,)  # Para que sea una tupla agremgamos una coma al final
+# Ejecutamos la sentencia con el objeto de cursor y el metodo execute (senstencia,valores)
 cursor.execute(sentencia_sql, valores)
+# Guardamos en la BD
 personas_db.commit()
 print('Se a eliminado el registro')
+# Cerramos los recursos
 cursor.close()
 personas_db.close()
